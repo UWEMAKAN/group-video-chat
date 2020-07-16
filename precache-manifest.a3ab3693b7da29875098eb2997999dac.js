0e3b8e3c7f61754da5b2be868e4fcd1f11f8a726
Uwem Akan Nkereuwem <uwemakan12@gmail.com>
2020-07-16 11:09:23 +0100

    Updates

---
+++ precache-manifest.a3ab3693b7da29875098eb2997999dac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3c54477090fc0320008bc996432ef90",
+    "url": "/group-video-chat/index.html"
+  },
+  {
+    "revision": "705440e6dcd267e06cca",
+    "url": "/group-video-chat/static/css/main.58e0922a.chunk.css"
+  },
+  {
+    "revision": "790c01fe3523430bd00e",
+    "url": "/group-video-chat/static/js/2.399c5be7.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/group-video-chat/static/js/2.399c5be7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "705440e6dcd267e06cca",
+    "url": "/group-video-chat/static/js/main.9c224544.chunk.js"
+  },
+  {
+    "revision": "0f0be471bdf49103fb6f",
+    "url": "/group-video-chat/static/js/runtime-main.652fb3e5.js"
+  }
+]);
